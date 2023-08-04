987635c3b790b542f290a2a32eac70ef0897698a
Wojciech Malarski <w.malarski@gmail.com>
2023-08-04 20:18:53 +0200

    adds typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import marko from "@marko/run/vite";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [marko()],
+  plugins: [marko(), tsconfigPaths()],
+  build: {
+    sourcemap: true, // Generate sourcemaps for all builds.
+    emptyOutDir: false, // Avoid server & client deleting files from each other.
+  },
 });
