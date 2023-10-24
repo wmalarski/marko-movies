c404600d13495823ee19aa7b73d147f200d03e0f
Wojciech Malarski <w.malarski@gmail.com>
2023-10-24 21:17:50 +0200

    some components moved to tags api

--- .marko-run/routes.d.ts
+++ .marko-run/routes.d.ts
@@ -3,8 +3,8 @@
   Do NOT manually edit this file or your changes will be lost.
 */
 
-import { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform } from "@marko/run/namespace";
-import type Run from "@marko/run";
+import type * as Run from "@marko/run";
+import { GetPaths, GetableHref, GetablePath, NotHandled, NotMatched, Platform, PostPaths, PostableHref, PostablePath } from "@marko/run/namespace";
 
 
 declare module "@marko/run" {
