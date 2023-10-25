38d88b3a4eb6c3001ac99d587619648ab36191fa
Wojciech Malarski <w.malarski@gmail.com>
2023-10-25 22:46:49 +0200

    adds netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
+import netlifyAdapter from "@marko/run-adapter-netlify";
 import marko from "@marko/run/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [marko(), tsconfigPaths()],
-  build: {
-    sourcemap: true, // Generate sourcemaps for all builds.
-    emptyOutDir: false, // Avoid server & client deleting files from each other.
-  },
+  plugins: [
+    marko({ adapter: netlifyAdapter({ edge: true }) }),
+    tsconfigPaths(),
+  ],
 });
